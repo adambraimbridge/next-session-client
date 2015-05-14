24d96abf9fbdc8f161cf0f0116bfb104aaf65fbf
Paul I Wilson <paul.i.wilson@ft.com>
2015-05-14 10:35:18 +0100

    Adding test app

--- test/app/main.js
+++ test/app/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 var session = require('../../main');
+
+function handleClick(e){
+	var target = e.target;
+	var method = target.getAttribute('data-method');
+	console.log('Call method ' + method);
+	session[method]().then(function(response){
+		console.log(response);
+	});
+}
+
+function addClick(el){
+	console.log('add click to', el);
+	el.addEventListener('click', handleClick);
+}
+
+[].slice.call(document.querySelectorAll('.buttons button'), 0).forEach(addClick);
+
+
