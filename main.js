40a46bc186180440f20cc0f87aee14d4ab74a8b8
Darren Hurley <ironsidevsquincy@googlemail.com>
2017-03-15 16:30:25 +0000

    Talk to session proxy

--- main.js
+++ main.js
@@ -1,55 +1,66 @@
-'use strict';
 const request = require('./src/request');
 const cache = require('./src/cache');
-const promises = {};
+const requests = {};
 
-function uuid (){
-	const uuid = cache('uuid')
-
-	if (uuid){
-		return Promise.resolve({
-			uuid:uuid
-		});
-	}
-	if (!promises.uuid) {
-		promises.uuid = request('/uuid').then(function (response){
-			cache('uuid', response.uuid);
-			return response;
-		});
+const getUuid = () => {
+	const cachedUUID = cache('uuid');
+	if (cachedUUID) {
+		return Promise.resolve({ uuid: cachedUUID });
 	}
 
-	return promises.uuid;
-}
+	const [, sessionId] = /FTSession_s=([^;]+)/.exec(document.cookie) || [];
+	if (!sessionId) {
+		return Promise.resolve({ uuid: undefined });
+	}
 
-function products () {
+	if (!requests.uuid) {
+		// pull out the session id from the FTSession_s cookie
+		requests.uuid = request(`/sessions/s/${sessionId}`)
+			.then(({ uuid } = {}) => {
+				delete requests.uuid;
+				if (uuid) {
+					cache('uuid', uuid);
+				}
+				return { uuid };
+			});
+	}
+
+	return requests.uuid;
+};
+
+const getProducts = () => {
 	const cachedProducts = cache('products');
 	const cachedUUID = cache('uuid');
-
-	if(cachedProducts && cachedUUID){
-		return Promise.resolve({products:cachedProducts, uuid:cachedUUID});
+	if (cachedProducts && cachedUUID){
+		return Promise.resolve({ products: cachedProducts, uuid: cachedUUID });
 	}
 
-	if (!promises.products) {
-		promises.products = request('/products').then(function (response) {
-			cache('products', response.products);
-			cache('uuid', response.uuid);
-			return response;
-		});
+	if (!requests.products) {
+		requests.products = request('/products')
+			.then(( { products, uuid } = {}) => {
+				delete requests.products;
+				if (products) {
+					cache('uuid', uuid);
+				}
+				if (uuid) {
+					cache('uuid', uuid);
+				}
+				return { products, uuid };
+			});
 	}
 
-	return promises.products;
-}
+	return requests.products;
+};
 
-function validate () {
-	return request('/validate');
-}
+const validate = () => {
+	return getUuid()
+		.then(response => response ? true : false);
+};
 
-module.exports = {
-	uuid : uuid,
-	validate : validate,
-	cache : cache,
-	products: products,
-	cookie : function () {
-		return (/FTSession=([^;]+)/.exec(document.cookie) || [null, ''])[1];
-	}
+export {
+	uuid: getUuid,
+	products: getProducts,
+	validate,
+	cache,
+	cookie: () => (/FTSession=([^;]+)/.exec(document.cookie) || [null, ''])[1]
 };
--- src/request.js
+++ src/request.js
@@ -1,38 +1,27 @@
-'use strict';
-
-function request (url) {
-	// if we don't have a session token cookie, don't bother..
-	if (document.cookie.indexOf('FTSession=') === -1) {
-		return Promise.reject(new Error('No session cookie found'));
-	}
-
+export default url => {
 	return fetch(`https://session-next.ft.com${url}`, {
-		credentials:'include',
+		credentials: 'include',
 		useCorsProxy: true
-	}).then(function (response) {
-		if (response.ok){
-			return (url === '/validate') ? Promise.resolve(true) : response.json();
-		} else {
-			return (url === '/validate') ? Promise.resolve(false) : Promise.reject(response.status);
-		}
-
-	}).catch(function (e) {
-		const message = e.message || '';
-		if (message.indexOf('timed out') > -1 || message.indexOf('Network request failed') > -1 || message.indexOf('Not Found') > -1) {
-			// HTTP timeouts and invalid sessions are a fact of life on the internet.
-			// We don't want to report this to Sentry.
-		} else {
+	})
+		.then(response => {
+			if (response.ok){
+				return response.json();
+			} else {
+				return response.text()
+					.then(text => {
+						throw new Error(`Next session responded with "${text}" (${response.status})`);
+					});
+			}
+		})
+		.catch(err => {
 			document.body.dispatchEvent(new CustomEvent('oErrors.log', {
 				bubbles: true,
 				detail: {
-					error: e,
+					error: err,
 					info: {
 						component: 'next-session-client'
 					}
 				}
-			}))
-		}
-	});
+			}));
+		});
 }
-
-module.exports = request;
